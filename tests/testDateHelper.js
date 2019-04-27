8be06c4bc5f99496c7851cdad8cd4e5717e8f0ac
fr.Innocent <brother.symeon@gmail.com>
2019-04-27 17:39:26 -0400

    Added data file for saints, tests, more npm packages and the like.

---
+++ tests/testDateHelper.js
@@ -0,0 +1,11 @@
+
+
+var assert = require("assert")
+var helper = require('../lib/dateHelper')
+describe('Helper', function() {
+  describe('#dayDiff', function() {
+    it('should return the diffence in days between two dates',function() {
+    
+    })
+  })
+})
